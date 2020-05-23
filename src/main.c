623c07b9c5e5c8aca8c4dd45b198d060ac64b97c
Roman Binkert <romanbinkert@gmail.com>
2020-05-23 17:17:10 +0200

    implemented coding challing II in main

--- src/main.c
+++ src/main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include "espl_lib.h" 
+
 
 /* main.c */
 int main(int argc, char *argv[]) {
-    printf("Hello ESPL");
+    unsigned int input_number;
+    char *number_as_word;
+    printf("Hello ESPL\n");
+    printf("Please enter a Number\n");
+    scanf("%u", &input_number);
+
+    number_as_word = num_to_words(input_number);
+    printf("You entered %s", number_as_word);
+
+    return 0;
 }
